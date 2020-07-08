bf2edb130946ec44b8e42b01227d8af27d6cee9c
MarcosPauloRMM <marcospaulomoura1999@gmail.com>
2020-07-08 20:26:59 -0300

    Base de dados

---
+++ db.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE historia
+(cd_historia INTEGER PRIMARY KEY NOT NULL,
+cd_leitor INTEGER NOT NULL,
+nm_historia TEXT NOT NULL,
+ds_genero TEXT NOT NULL,
+ds_sinopse TEXT NOT NULL,
+ds_conteudo TEXT NOT NULL,
+constraint leitor_historia_fk foreign key(cd_leitor)
+references leitor (cd_leitor))
+
+CREATE TABLE leitor
+(cd_leitor INTEGER PRIMARY KEY NOT NULL,
+nm_leitor TEXT NOT NULL,
+ds_email TEXT NOT NULL,
+ds_senha INTEGER NOT NULL)
